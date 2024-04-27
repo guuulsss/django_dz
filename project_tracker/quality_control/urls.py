99b0e0159a5dd995be4a1aa976eeb27cd6f1fc4f
guuulsss <alesi-ia@yandex.ru>
2024-04-28 02:26:46 +0300

    Scnd

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -4,16 +4,16 @@ from quality_control import views
 app_name = 'quality_control'
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-    #path('bugs/', views.bug_report_list, name='bug_report_list'),
-    #path('features/', views.feature_list, name='feature_list'),
-    #path('bugs/<int:bug_id>/', views.bug_report_detail, name='bug_report_detail'),
-    #path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),
+    path('', views.index, name='index'),
+    path('bugs/', views.bug_report_list, name='bug_report_list'),
+    path('features/', views.feature_list, name='feature_list'),
+    path('bugs/<int:bug_id>/', views.bug_report_detail, name='bug_report_detail'),
+    path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),
 
-    path('', views.IndexView.as_view(), name='index'),
-    path('bugs/', views.BugReportListView.as_view(), name='bug_report_list'),
-    path('features/', views.FeatureRequestListView.as_view(), name='feature_list'),
-    path('bugs/<int:bug_id>/', views.BugReportDetailView.as_view(), name='bug_report_detail'),
-    path('features/<int:feature_id>/', views.FeatureRequestDetailView.as_view(), name='feature_detail'),
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('bugs/', views.BugReportListView.as_view(), name='bug_report_list'),
+    # path('features/', views.FeatureRequestListView.as_view(), name='feature_list'),
+    # path('bugs/<int:bug_id>/', views.BugReportDetailView.as_view(), name='bug_report_detail'),
+    # path('features/<int:feature_id>/', views.FeatureRequestDetailView.as_view(), name='feature_detail'),
 
 ]
--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -6,42 +6,58 @@ from quality_control.models import BugReport, FeatureRequest
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    bugs_url = reverse('quality_control:bug_report_list')
-    features_url = reverse('quality_control:feature_list')
-    html = f"<h1>Система контроля качества</h1>"
-    html += f"<a href='{bugs_url}'>Список всех багов</a>"
-    html += f"<br><a href='{features_url}'>Запросы на улучшение</a>"
-    return HttpResponse(html)
+    return render(request,'quality_control/index.html')
+# def index(request):
+#     bugs_url = reverse('quality_control:bug_report_list')
+#     features_url = reverse('quality_control:feature_list')
+#     html = f"<h1>Система контроля качества</h1>"
+#     html += f"<a href='{bugs_url}'>Список всех багов</a>"
+#     html += f"<br><a href='{features_url}'>Запросы на улучшение</a>"
+#     return HttpResponse(html)
 
 def bug_report_list(request):
     bug_reports = BugReport.objects.all()
-    html = '<h1>Список всех Багов</h1>'
-    for bug_report in bug_reports:
-        html += f'<li><a href="{bug_report.id}/">{bug_report.title}</a></li>'
-    return HttpResponse(html)
+    return render(request, 'quality_control/bug_report_list.html', {'bug_report_list': bug_reports})
 
+# def bug_report_list(request):
+#     bug_reports = BugReport.objects.all()
+#     html = '<h1>Список всех Багов</h1>'
+#     for bug_report in bug_reports:
+#         html += f'<li><a href="{bug_report.id}/">{bug_report.title}</a></li>'
+#     return HttpResponse(html)
 def bug_report_detail(request, bug_id):
-    bug_report = get_object_or_404(BugReport, id=bug_id)
-    response_html = f'<h1>{bug_report.title}</h1>'
-    response_html += f'<p>Status: {bug_report.status}</p>'
-    response_html += f'<p>Description: {bug_report.description}</p>'
-    return HttpResponse(response_html)
+    bug_reports = get_object_or_404(BugReport, id=bug_id)
+    return render(request, 'quality_control/bug_report_detail.html', {'bug_report_list': bug_reports})
 
+# def bug_report_detail(request, bug_id):
+#     bug_report = get_object_or_404(BugReport, id=bug_id)
+#     response_html = f'<h1>{bug_report.title}</h1>'
+#     response_html += f'<p>Status: {bug_report.status}</p>'
+#     response_html += f'<p>Description: {bug_report.description}</p>'
+#     return HttpResponse(response_html)
 
 def feature_list(request):
     features = FeatureRequest.objects.all()
-    html = '<h1>Список всех Запросов на улучшение</h1>'
-    for feature in features:
-        html += f'<li><a href="{feature.id}/">{feature.title}</a></li>'
-    return HttpResponse(html)
+    return render(request, 'quality_control/feature_list.html', {'features': features})
+
+# def feature_list(request):
+#     features = FeatureRequest.objects.all()
+#     html = '<h1>Список всех Запросов на улучшение</h1>'
+#     for feature in features:
+#         html += f'<li><a href="{feature.id}/">{feature.title}</a></li>'
+#     return HttpResponse(html)
 
 def feature_detail(request, feature_id):
-    feature = get_object_or_404(FeatureRequest, id=feature_id)
-    response_html = f'<h1>{feature.title}</h1>'
-    response_html += f'<p>Status: {feature.status}</p>'
-    response_html += f'<p>Priority: {feature.priority}</p>'
-    response_html += f'<p>Description: {feature.description}</p>'
-    return HttpResponse(response_html)
+    features = get_object_or_404(FeatureRequest, id=feature_id)
+    return render(request, 'quality_control/feature_detail.html', {'features': features})
+
+# def feature_detail(request, feature_id):
+#     feature = get_object_or_404(FeatureRequest, id=feature_id)
+#     response_html = f'<h1>{feature.title}</h1>'
+#     response_html += f'<p>Status: {feature.status}</p>'
+#     response_html += f'<p>Priority: {feature.priority}</p>'
+#     response_html += f'<p>Description: {feature.description}</p>'
+#     return HttpResponse(response_html)
 
 #def quality_control_index(request):
 #    return HttpResponse("<h1>Главная страница приложения quality_control</h1>")
