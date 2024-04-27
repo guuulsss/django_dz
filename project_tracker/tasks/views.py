965fa8b0e93f7e90f049fe294c99fa712c4da955
guuulsss <alesi-ia@yandex.ru>
2024-04-28 02:55:15 +0300

    Thrd

--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -57,31 +57,38 @@ from django.views import View
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        projects_list_url = reverse('tasks:projects_list')
-        quality_control_index_url = reverse('quality_control:index')
+        return render(request, 'tasks/index.html')
 
-        html = f"<h1>Страница приложения tasks</h1>"
-        html += f"<a href='{projects_list_url}'>Список проектов</a><br>"
-        html += f"<a href='{quality_control_index_url}'>Перейти на главную страницу приложения quality_control</a>"
-
-        return HttpResponse(html)
+# class IndexView(View):
+#     def get(self, request, *args, **kwargs):
+#         projects_list_url = reverse('tasks:projects_list')
+#         quality_control_index_url = reverse('quality_control:index')
+#
+#         html = f"<h1>Страница приложения tasks</h1>"
+#         html += f"<a href='{projects_list_url}'>Список проектов</a><br>"
+#         html += f"<a href='{quality_control_index_url}'>Перейти на главную страницу приложения quality_control</a>"
+#
+#         return HttpResponse(html)
 
 from django.views.generic import ListView
 
-
 class ProjectsListView(ListView):
     model = Project
+    template_name = 'tasks/projects_list.html'
 
-    def get(self, request, *args, **kwargs):
-        projects = self.get_queryset()
-        projects_html = '<h1>Список проектов</h1>'
-
-
-
-        for project in projects:
-            projects_html += f'<li><a href="{project.id}/">{project.name}</a></li>'
-        projects_html += '</ul>'
-        return HttpResponse(projects_html)
+# class ProjectsListView(ListView):
+#     model = Project
+#
+#     def get(self, request, *args, **kwargs):
+#         projects = self.get_queryset()
+#         projects_html = '<h1>Список проектов</h1>'
+#
+#
+#
+#         for project in projects:
+#             projects_html += f'<li><a href="{project.id}/">{project.name}</a></li>'
+#         projects_html += '</ul>'
+#         return HttpResponse(projects_html)
 
 
 from django.views.generic import DetailView
@@ -89,26 +96,36 @@ from django.views.generic import DetailView
 class ProjectDetailView(DetailView):
     model = Project
     pk_url_kwarg = 'project_id'
+    template_name = 'tasks/project_detail.html'
 
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        project = self.object
-        tasks = project.tasks.all()
-        response_html = f'<h1>{project.name}</h1><p>{project.description}</p>'
-        response_html += '<h2>Задачи</h2><ul>'
-        for task in tasks:
-            response_html += f'<li><a href="tasks/{task.id}/">{task.name}</a></li>'
-        response_html += '</ul>'
-        return HttpResponse(response_html)
+# class ProjectDetailView(DetailView):
+#     model = Project
+#     pk_url_kwarg = 'project_id'
+#
+#     def get(self, request, *args, **kwargs):
+#         self.object = self.get_object()
+#         project = self.object
+#         tasks = project.tasks.all()
+#         response_html = f'<h1>{project.name}</h1><p>{project.description}</p>'
+#         response_html += '<h2>Задачи</h2><ul>'
+#         for task in tasks:
+#             response_html += f'<li><a href="tasks/{task.id}/">{task.name}</a></li>'
+#         response_html += '</ul>'
+#         return HttpResponse(response_html)
 
 class TaskDetailView(DetailView):
     model = Task
     pk_url_kwarg = 'task_id'
+    template_name = 'tasks/task_detail.html'
 
-    def get(self, request, *args, **kwargs):
-        task = self.get_object()
-        response_html = f'<h1>{task.name}</h1><p>{task.description}</p>'
-        return HttpResponse(response_html)
+# class TaskDetailView(DetailView):
+#     model = Task
+#     pk_url_kwarg = 'task_id'
+#
+#     def get(self, request, *args, **kwargs):
+#         task = self.get_object()
+#         response_html = f'<h1>{task.name}</h1><p>{task.description}</p>'
+#         return HttpResponse(response_html)
 
 
 
