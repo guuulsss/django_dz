8900d357bb2e2597558341ef8dd109d8ece7b4bb
guuulsss <alesi-ia@yandex.ru>
2024-04-29 19:28:28 +0300

    Final

--- project_tracker/project_tracker/urls.py
+++ project_tracker/project_tracker/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from tasks.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', index),
     path('tasks/', include('tasks.urls')),
     path('quality_control/', include('quality_control.urls')),
-
+    path('admin/', admin.site.urls),
 ]
